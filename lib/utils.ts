79e4c265f267f7ced4843a67c9d469ae29e0d4bf
DaniloMDL1 <danilodfl016@gmail.com>
2025-12-10 13:27:19 +0100

    fix:touppercase

--- lib/utils.ts
+++ lib/utils.ts
@@ -6,7 +6,7 @@ export function cn(...inputs: ClassValue[]) {
 }
 
 export const getInitials = (name: string) => {
-    const words = name.split(" ")
+    const words = name.split(" ").filter((word) => word.length > 0)
     if(words.length === 1) return words[0][0].toUpperCase()
     return words[0][0].toUpperCase() + words[words.length - 1][0].toUpperCase()
 }
